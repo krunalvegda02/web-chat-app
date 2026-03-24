e376235ec61f904bdb349e2a9e7528d14364c106
stzkdigitalmedia <Krunal.vegda02@gmail.com>
2026-03-24 19:56:38 +0530

    ios issue

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    legacy({
+      targets: ['ios >= 13', 'safari >= 13'],
+      additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
+    }),
   ],
   server: {
     port: 5173,
